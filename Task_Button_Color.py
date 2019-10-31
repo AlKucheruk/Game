5e8b9364aa452e9d8c242978e3a4b20b62232d4f
AlKucheruk <Aleksei_K@list.ru>
2019-10-31 13:18:08 +0500

    3

--- Task_Button_Color.py
+++ Task_Button_Color.py
@@ -1,33 +1,36 @@
 #import tkinter as tk
 from tkinter import *
 
-
-#colors_name = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'purple']
-colors = ['#ff0000', '#ff7d00', '#ffff00', '#00ff00', '#007dff', '#0000ff', '#7d00ff']
-colors_2={'#ff0000':"красный", '#ff7d00':"оранжевый", '#ffff00':"желтый", '#00ff00':"зелёный", '#007dff':"голубой", '#0000ff':"синий", '#7d00ff':"фиолетовый"}
+colors_s = ['#ff0000', '#ff7d00', '#ffff00', '#00ff00', '#007dff', '#0000ff', '#7d00ff']
+colors_d={'#ff0000':"red", '#ff7d00':"orange", '#ffff00':"yellow", '#00ff00':"green", '#007dff':"cyan", '#0000ff':"blue", '#7d00ff':"purple"}
 
 class Color_Button:
     """ в этом классе делаю обработчик который принимает цвет и согласно этого цвета отрисовывается """
 
     def __init__(self, master, color):
         self.color = color
-        self.button_v = Button(master, width=20, height=2, bg=color, command=self.push_button)
-        self.button_v.pack()
+        self.button_v = Button(master, width=5, height=1, bg=color, command=self.push_button)
+        self.button_v.pack(side=LEFT)
     def push_button(self):
         entry_v.delete(0, END)
         entry_v.insert(0, self.color)
-        label_v['text'] = colors_2[self.color]
+        label_v['text'] = colors_d[self.color]
         return self.color
 
 root = Tk()
+fr_top = LabelFrame(root, text='Top')
+fr_bottom = LabelFrame(root, text='Bottom')
 
-label_v = Label(width=20, height=2, text='')
-label_v.pack()
+fr_top.pack()
+fr_bottom.pack()
 
-entry_v = Entry(width=25, justify='center')
-entry_v.pack()
+label_v = Label(fr_top, width=20, height=1, text='')
+label_v.pack(side=TOP, padx=10, pady=10)
 
-for color in colors:
-    Color_Button(root, color)
+entry_v = Entry(fr_top, width=25, justify='center')
+entry_v.pack(side=TOP, padx=10, pady=10)
 
+for color in colors_s:
+    Color_Button(fr_bottom, color)
+    
 root.mainloop()
