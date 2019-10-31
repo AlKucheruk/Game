3cdf34213955fea1a382d46fc5bba77a50c99260
AlKucheruk <Aleksei_K@list.ru>
2019-10-31 09:45:11 +0500

    simple

--- Task_Button_Color.py
+++ Task_Button_Color.py
@@ -1,34 +1,33 @@
-import tkinter as tk
+#import tkinter as tk
+from tkinter import *
+
+
+#colors_name = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'purple']
+colors = ['#ff0000', '#ff7d00', '#ffff00', '#00ff00', '#007dff', '#0000ff', '#7d00ff']
+colors_2={'#ff0000':"красный", '#ff7d00':"оранжевый", '#ffff00':"желтый", '#00ff00':"зелёный", '#007dff':"голубой", '#0000ff':"синий", '#7d00ff':"фиолетовый"}
 
 class Color_Button:
     """ в этом классе делаю обработчик который принимает цвет и согласно этого цвета отрисовывается """
 
-    def __init__(self, master, color_v, color_n):
-        self.button_v = tk.Button(master, width=20, height=2, bg=color_v, command=self.push_button)
-        self.color = color_v
-        self.color_n = color_n
+    def __init__(self, master, color):
+        self.color = color
+        self.button_v = Button(master, width=20, height=2, bg=color, command=self.push_button)
         self.button_v.pack()
-
     def push_button(self):
         entry_v.delete(0, END)
         entry_v.insert(0, self.color)
-        label_v['text'] = self.color_n
+        label_v['text'] = colors_2[self.color]
         return self.color
 
+root = Tk()
 
-colors = ['#ff0000', '#ff7d00', '#ffff00', '#00ff00', '#007dff', '#0000ff', '#7d00ff']
-colors_name = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'purple']
-
-root = tk.Tk()
-
-label_v = tk.Label(width=20, height=2)
-label_v['text'] = 'Samfing'
+label_v = Label(width=20, height=2, text='')
 label_v.pack()
 
-entry_v = tk.Entry(width=20, justify='center')
+entry_v = Entry(width=25, justify='center')
 entry_v.pack()
 
 for color in colors:
-    Color_Button(root, color, 'yellow')
+    Color_Button(root, color)
 
 root.mainloop()
